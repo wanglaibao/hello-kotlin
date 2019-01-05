c3f25300c92143587a7a1a6b6bb5e3d273a8ff30
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-01-05 18:51:08 +0800

    update pom.xml file info

--- src/main/kotlin/com/laibao/kotlin/chapter01/StringTypeValue.kt
+++ src/main/kotlin/com/laibao/kotlin/chapter01/StringTypeValue.kt
@@ -21,4 +21,50 @@ fun main(args: Array<String>) {
     val x = 2
     val y = 3
     println("$x times $y is ${x * y}.")
+
+
+    /**
+     * Multiline strings
+     */
+
+    val speech0 = """Four score and
+               seven years ago
+               our fathers ..."""
+
+    println(speech0)
+    println()
+
+
+    val speech1 = """Four score and
+               |seven years ago
+               |our fathers ...""".trimMargin()
+    println(speech1)
+    println()
+
+    /**
+     * How to convert a list to a String
+     */
+    val nums = listOf(1,2,3,4,5)
+    println(nums)
+
+    val numstrings = nums.joinToString()
+    println(numstrings)
+    println()
+
+    val numstrings1 = nums.joinToString(
+            separator = ", ",
+            prefix = "[",
+            postfix = "]",
+            limit = 3,
+            truncated = "there’s more ..."
+    )
+    println(numstrings1)
+    println()
+
+    val wordsArray = arrayOf("Al", "was", "here")
+    val wordsArrayToString = wordsArray.joinToString("||")
+    println(wordsArrayToString)
+
+    val wordsArrayToString1 = wordsArray.joinToString(separator = " ")
+    println(wordsArrayToString1)
 }
