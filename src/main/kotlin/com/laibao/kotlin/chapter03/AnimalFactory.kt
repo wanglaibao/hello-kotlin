431b86d79e4d1bd920ac50baa8ed7cf7e435fb91
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-01-09 09:53:40 +0800

    update pom.xml file info

--- src/main/kotlin/com/laibao/kotlin/chapter03/AnimalFactory.kt
+++ src/main/kotlin/com/laibao/kotlin/chapter03/AnimalFactory.kt
@@ -4,10 +4,40 @@ fun main(args: Array<String>) {
     val c = catFactory()
     // Indeed prints "Cat"
     println(c.name)
+
+    val animalTypes = listOf("dog", "dog", "cat", "dog", "cat", "cat")
+
+    for (animal in animalTypes) {
+        val c = animalFactory(animal)
+        println(c.name)
+    }
 }
 
-class Cat {
-    val name = "Cat"
+interface Animal {
+    val name : String
 }
+
+class Cat : Animal{
+    override val name = "Cat"
+}
+
+
+class Dog : Animal{
+    override val name = "Dog"
+}
+
 
 fun catFactory() : Cat = Cat()
+
+
+fun animalFactory(animalType: String) : Animal {
+    return  when(animalType.toLowerCase()) {
+                    "cat" -> Cat()
+                    "dog" -> Dog()
+                    else -> throw RuntimeException("Unknown animal $animalType")
+    }
+}
+
+
+
+
