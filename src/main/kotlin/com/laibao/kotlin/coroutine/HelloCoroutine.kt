170fea7d9012bd16a345b64eb6af27741545037b
wanglaibao2010 <wanglaibao2010@gmail.com>
2019-03-06 13:53:05 +0800

    update pom.xml file info

--- src/main/kotlin/com/laibao/kotlin/coroutine/HelloCoroutine.kt
+++ src/main/kotlin/com/laibao/kotlin/coroutine/HelloCoroutine.kt
@@ -3,9 +3,18 @@ package com.laibao.kotlin.coroutine
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 fun main() {
+
+    // Suspend functions are only allowed to be called from a coroutine or another suspend function
+    // delay(1000)
+
     println("Start")
+
+    runBlocking {
+        delay(2000)
+    }
 
     //Start a coroutine
     GlobalScope.launch {
